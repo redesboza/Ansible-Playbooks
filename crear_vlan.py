704dbc17c64b812f65ca10b3c00e5eef0524f242
redesboza <redesboza@gmail.com>
2026-01-21 12:54:52 -0500

    Update crear_vlan.py

--- crear_vlan.py
+++ crear_vlan.py
@@ -4,36 +4,32 @@ import sys
 from netmiko import ConnectHandler
 import os
 
-# Argumentos desde el playbook
-vlan_id = sys.argv[1]
-vlan_name = sys.argv[2]
-
-# AWX pasa automáticamente estas variables de entorno si usas credenciales SSH
-host_ip = os.environ.get("ANSIBLE_HOST")
-username = os.environ.get("ANSIBLE_NET_USERNAME") or os.environ.get("ANSIBLE_USER")
-password = os.environ.get("ANSIBLE_NET_PASSWORD") or os.environ.get("ANSIBLE_PASSWORD")
+host = sys.argv[1]
+username = sys.argv[2]
+password = sys.argv[3]
+port = sys.argv[4]
+vlan_id = sys.argv[5]
+nombre_vlan = sys.argv[6]
 
 device = {
-    "device_type": "cisco_s300",  # Para SG350
-    "host": host_ip,
-    "username": username,
-    "password": password,
-    "port": 11110,  # Puerto SSH personalizado
+    'device_type': 'cisco_s300',
+    'host': host,
+    'username': username,
+    'password': password,
+    'port': port,
 }
 
+net_connect = ConnectHandler(**device)
+
 commands = [
+    f"vlan database",
     f"vlan {vlan_id}",
-    f"name {vlan_name}",
-    "exit",
-    "do wr mem"
+    f"exit",
+    f"interface vlan {vlan_id}",
+    f"name {nombre_vlan}",
+    f"exit"
 ]
 
-try:
-    connection = ConnectHandler(**device)
-    output = connection.send_config_set(commands)
-    print(output)
-    connection.disconnect()
-except Exception as e:
-    print(f"ERROR: {str(e)}")
-    sys.exit(1)
-
+output = net_connect.send_config_set(commands)
+print(output)
+net_connect.disconnect()
